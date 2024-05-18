e08c9f9b9af68f8ff575fc42d630ca6f72226374
ardririy <hynnkgw27@gmail.com>
2024-05-19 07:53:40 +0900

    精進

--- contests/abc354/src/bin/f.rs
+++ contests/abc354/src/bin/f.rs
@@ -1,9 +1,65 @@
+use std::collections::BTreeSet;
+
+use itertools::Itertools;
 use proconio::input;
 
 fn solve() {
     input! {
+        n: usize,
+        a: [u64; n]
     }
 
+    let (_, mut r) = a.iter().enumerate().fold(
+        (vec![INF; n], vec![INF as usize; n]),
+        |(mut dp, mut id), (idx, &x)| {
+            let mut ng = !0;
+            let mut ok = n;
+            while (ok.wrapping_sub(ng)) > 1 {
+                let mid = (ok.wrapping_add(ng)) / 2;
+                if dp[mid] >= x {
+                    ok = mid;
+                } else {
+                    ng = mid;
+                }
+            }
+            id[idx] = ok;
+            dp[ok] = x;
+            (dp, id)
+        },
+    );
+
+    let mut b = a.iter().map(|x| INF - *x).collect_vec();
+    b.reverse();
+    let (_, mut l) = b.iter().enumerate().fold(
+        (vec![INF; n], vec![INF as usize; n]),
+        |(mut dp, mut id), (idx, &x)| {
+            let mut ng = !0;
+            let mut ok = n;
+            while (ok.wrapping_sub(ng)) > 1 {
+                let mid = (ok.wrapping_add(ng)) / 2;
+                if dp[mid] >= x {
+                    ok = mid;
+                } else {
+                    ng = mid;
+                }
+            }
+            id[idx] = ok;
+            dp[ok] = x;
+            (dp, id)
+        },
+    );
+
+    l.reverse();
+    let lis = r.iter().max().unwrap();
+    let mut ans = vec![];
+    for i in 0..n {
+        if l[i] + r[i] + 1 == *lis + 1 {
+            ans.push(i + 1);
+        }
+    }
+    println!("{}", ans.len());
+    println!("{}", ans.iter().join(" "));
+    println!("{:?} {:?}", l, r);
 }
 
 /*
@@ -16,7 +72,6 @@ fn solve() {
 
 */
 
-
 static INF: u64 = 1e18 as u64;
 
 trait ChLibs<T: std::cmp::Ord> {
@@ -26,26 +81,27 @@ trait ChLibs<T: std::cmp::Ord> {
 
 impl<T: std::cmp::Ord> ChLibs<T> for T {
     fn chmin(&mut self, elm: T) -> bool {
-        return
-            if *self > elm {
-                *self = elm;
-                true
-            } else { false };
+        return if *self > elm {
+            *self = elm;
+            true
+        } else {
+            false
+        };
     }
 
     fn chmax(&mut self, elm: T) -> bool {
-        return
-            if *self < elm {
-                *self = elm;
-                true
-            } else { false };
+        return if *self < elm {
+            *self = elm;
+            true
+        } else {
+            false
+        };
     }
 }
 
-
 fn main() {
-    // input! { i: usize }
-    let mut i = 1;
+    input! { mut i: usize }
+    // let mut i = 1;
     while i != 0 {
         solve();
         i -= 1;
