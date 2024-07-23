54fa23466fee0a88e8cbdb7431663abc43af1e92
ardririy <hynnkgw27@gmail.com>
2024-07-24 00:51:20 +0900

    add all

--- contests/abc363/src/bin/e.rs
+++ contests/abc363/src/bin/e.rs
@@ -1,7 +1,50 @@
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 use proconio::{input};
 fn solve() {
     input!{
-        
+        h: usize,
+        w: usize,
+        y: u64,
+        a: [[u64; w]; h]
+    }
+
+    let mut seen = vec![vec![false; w]; h];
+    let mut pq: BinaryHeap<(Reverse<u64>, usize, usize)> = BinaryHeap::new();
+    for i in 0..h {
+        for j in 0..w {
+            if (i == 0 || i == h-1) || (j == 0 || j == w-1) {
+                seen[i][j] = true;
+                pq.push((Reverse(a[i][j]), i, j));
+            } 
+        }
+    }
+
+    let di = &[!0, 0, 1, 0];
+    let dj = &[0, !0, 0, 1];
+
+    let mut current = h * w;
+    for yi in 1..=y {
+        while let Some((Reverse(height), pi, pj)) = pq.pop() {
+            if height > yi {
+                pq.push((Reverse(height), pi, pj));
+                break;
+            }
+            current -= 1;
+
+            for r in 0..4 {
+                let ni = pi.wrapping_add(di[r]);
+                let nj = pj.wrapping_add(dj[r]);
+
+                if ni < h && nj < w && !seen[ni][nj] {
+                    seen[ni][nj] = true;
+                    pq.push((Reverse(a[ni][nj]), ni, nj));
+                }
+            }
+
+        }
+
+        println!("{current}");
     }
 }
 
