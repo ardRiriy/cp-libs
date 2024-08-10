8c0f6eb594c50b5fd3c287e24e38c26f00e77a1c
ardririy <hynnkgw27@gmail.com>
2024-08-10 22:58:26 +0900

    重実装

---
+++ contests/abc366/src/bin/b.rs
@@ -0,0 +1,98 @@
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
+fn solve() {
+    input!{
+        n: usize,
+        s: [Chars; n]
+    }
+
+    let mut ans = vec![vec![]; 100];
+    for i in 0..n {
+        for j in 0..s[i].len() {
+            ans[j].push(s[i][j]);
+        }
+        for j in s[i].len()..100 {
+            ans[j].push('*');
+        }
+    }
+
+    for i in 0..100 {
+        ans[i].reverse();
+
+        if ans[i].iter().all(|c| *c == '*') {
+            continue;
+        }
+        while !ans[i].is_empty() && *ans[i].last().unwrap() == '*' {
+            ans[i].pop();
+        }
+        println!("{}", ans[i].iter().join(""));
+    }
+}
+
+/*
+鹿のardririy, arDeeriy
+　　　　　　　　　　　　 　 　 　 　 　 　 ／
+　　　　　　　　　　　　　　　　　 　 　 //
+　　　　　　　　　 　 　 　 　 　 　 　 //　 　 　 　 |:
+　　　　　　　　　　　　　　　　　 　 // 　 　 　 　 .|i
+　　　　　　　　　 　 　 　 　 　 　 //　 　 　 　 　 ||
+　　　　　　　　　　　　　　　　　　l i　　　　　　 　 ||
+　　　　　.　´￣￣｀ｰ　、　　　　 | l　 　 　 　 　 ∥
+　　　 ／　 . 　 　 　_＞─- ､ 　 l |　　　　　　 .∥
+　　　i　 〆　 　 ／　 　 　 　 ＼.i | 　 　 　 　 ∥
+　　　| /　 　 ／ 　 　 　 　 　 　 l |　ﾊ　　　　.′
+　 　 ; :　 　 .′　　　　　　　　　 ヾゝi !　　　/.′
+　　 《　　　 :　　　 　 　 r─-､　　 i ﾘ:l　　 //　　　　 　 　 、
+　 　 |　　　　 　 　 　 　 ｀ヾ.　＼ r‐’:ﾚ=‐' .ゝ...ノ＿＿＼＿))
+　　 弋　 　 　 　 　 　 　 　 ∨ソ`ー''`ー---一　‐─‐-､)¨´
+　　 　 } 、　　　　　　　　　　 i_..ノ _,　　　　ハ'
+　　 　 ∨＞､　iゝ.. 　 　 　 　 　 弋);;,ゞ..ノ　.′
+　　　　 ∨::::| 7!:::..ヾ.　　　　.′ヽ.　` 　 　 /
+　　　　　∨::|.′::::::ixxr, 　 /　　　:　　　　 I.
+　 　 　 　 }:::|..:ｉ..::::::|　i,,　　"'' ´ヾ.i 　 　 　 ﾊ
+　　　　 　 ::::|:::|::::::/　 }ヾ.　　　　ﾐゝ.　 ,、..:::::)
+　　　　 　 i:::|ヾ:::::i　 /.::::|｀ヾ..,,　..ノ＼ヾ..＿/
+　 　 　 　 |:::l　}:. {　 |::::..′　　　　　　 `ー''
+　 　 　 　 |:::| /.:/l　 !.:::l
+　　　　　 ﾉ..:ﾚ.:::ｉ::l　ﾉ..::|
+　　　 　 (人7.::::|:::V.::::::|
+　　　　　`''/ .:::::!ｰ:::::::::ﾊ
+　　　　　 /..::::::::| (_/＼__)
+　　　 　 (__/(,＿)
+*/
+
+static INF: u64 = 1e18 as u64;
+
+trait ChLibs<T: std::cmp::Ord> {
+    fn chmin(&mut self, elm: T) -> bool;
+    fn chmax(&mut self, elm: T) -> bool;
+}
+
+impl<T: std::cmp::Ord> ChLibs<T> for T {
+    fn chmin(&mut self, elm: T) -> bool {
+        return if *self > elm {
+            *self = elm;
+            true
+        } else {
+            false
+        };
+    }
+
+    fn chmax(&mut self, elm: T) -> bool {
+        return if *self < elm {
+            *self = elm;
+            true
+        } else {
+            false
+        };
+    }
+}
+
+fn main() {
+    // input! { mut i: usize }
+    let mut i = 1;
+    while i != 0 {
+        solve();
+        i -= 1;
+    }
+}
