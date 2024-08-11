f9ecf5a56f4a06bad2b5e335219b24624c8827ed
ardririy <hynnkgw27@gmail.com>
2024-08-12 05:38:52 +0900

    lib dirをcontest/いかに生やしてworkspace経由でロード可能にした

---
+++ contests/cps/src/chlibs.rs
@@ -0,0 +1,24 @@
+pub trait ChLibs<T: std::cmp::Ord> {
+    fn chmin(&mut self, elm: T) -> bool;
+    fn chmax(&mut self, elm: T) -> bool;
+}
+
+impl<T: std::cmp::Ord> ChLibs<T> for T {
+    fn chmin(&mut self, elm: T) -> bool {
+        return if *self > elm {
+            *self = elm;
+            true
+        } else {
+            false
+        };
+    }
+
+    fn chmax(&mut self, elm: T) -> bool {
+        return if *self < elm {
+            *self = elm;
+            true
+        } else {
+            false
+        };
+    }
+}
