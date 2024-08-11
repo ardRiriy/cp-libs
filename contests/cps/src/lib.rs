5b6321d771c2d606c0b5dff414fea78e69d2d520
ardririy <hynnkgw27@gmail.com>
2024-08-12 06:13:52 +0900

    librariesをコンテスト中に使いやすいようcpsに移行

--- contests/cps/src/lib.rs
+++ contests/cps/src/lib.rs
@@ -1,3 +1,5 @@
 pub mod example;
 pub mod chlibs;
 pub mod consts;
+pub mod cumulative_sum;
+pub mod prime;
---
+++ contests/cps/src/prime.rs
@@ -0,0 +1,48 @@
+// 素因数分解
+/* n = 1e12ぐらいまでじゃないとTLEするので注意 */
+pub fn prime_factorization(n: u64) -> std::collections::BTreeMap<u64, u64> {
+    let mut res = std::collections::BTreeMap::new();
+
+    let mut i = 2;
+    let mut k = n;
+    while i * i <= n {
+        let mut cnt = 0;
+        while k % i == 0 {
+            k /= i;
+            cnt += 1;
+        }
+        if cnt != 0 {
+            res.insert(i, cnt);
+        }
+        i += 1;
+    }
+
+    if k != 1 {
+        res.insert(k, 1);
+    }
+
+    res
+}
+
+/* nまでの素数を生成して配列で返す */
+pub fn create_primes(n: usize) -> Vec<u64> {
+    let mut res = vec![];
+
+    let n = 1e6 as usize + 2;
+    let mut ck = vec![false; n];
+
+    let mut i = 2;
+    while i < n {
+        if !ck[i] {
+            res.push(i as u64);
+            let mut j = i;
+            while j < n {
+                ck[j] = true;
+                j += i;
+            }
+        }
+        i += 1;
+    }
+
+    res
+}
