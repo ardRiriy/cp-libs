fe53032975762e1a54c571b74789abd6a2c1e3b7
ardririy <hynnkgw27@gmail.com>
2024-08-12 10:37:39 +0900

    文字列の前半より辞書順で小さいものを前半におくと必ず作れて、文字列の前半と全く同じものが作れるかどうかが焦点

---
+++ contests/abc242/src/bin/e.rs
@@ -0,0 +1,79 @@
+use ac_library::ModInt998244353;
+use proconio::{input, marker::Chars};
+fn main() {
+    input!{
+        mut t: i32,
+    }
+
+    for _ in 0..t {
+        solve();
+    }
+}
+
+fn solve() {
+    input! {
+        n: usize,
+        s: Chars,
+    }
+
+    let mut ans = ModInt998244353::new(0);
+    let base =  ModInt998244353::new(26);
+
+    let check_len = n / 2 + n % 2;
+
+    for i in 0..check_len {
+        let p = s[i] as u64 - 'A' as u64;
+        ans += base.pow(check_len as u64 - (i + 1) as u64) * p;
+    }
+
+    let mut flg = true;
+    for i in (0..check_len).rev() {
+        let rev_i = n - i - 1;
+        if (s[i] as u8) < s[rev_i] as u8{
+            ans += 1;
+            flg = false;
+            break;
+        } else if (s[i] as u8) > s[rev_i] as u8 {
+            flg = false;
+            break;
+        }
+    }
+
+    if flg {
+        ans += 1;
+    }
+
+    println!("{}", ans);
+}
+
+/*
+鹿のardririy, arDeeriy
+　　　　　　　　　　　　 　 　 　 　 　 　 ／
+　　　　　　　　　　　　　　　　　 　 　 //
+　　　　　　　　　 　 　 　 　 　 　 　 //　 　 　 　 |:
+　　　　　　　　　　　　　　　　　 　 // 　 　 　 　 .|i
+　　　　　　　　　 　 　 　 　 　 　 //　 　 　 　 　 ||
+　　　　　　　　　　　　　　　　　　l i　　　　　　 　 ||
+　　　　　.　´￣￣｀ｰ　、　　　　 | l　 　 　 　 　 ∥
+　　　 ／　 . 　 　 　_＞─- ､ 　 l |　　　　　　 .∥
+　　　i　 〆　 　 ／　 　 　 　 ＼.i | 　 　 　 　 ∥
+　　　| /　 　 ／ 　 　 　 　 　 　 l |　ﾊ　　　　.′
+　 　 ; :　 　 .′　　　　　　　　　 ヾゝi !　　　/.′
+　　 《　　　 :　　　 　 　 r─-､　　 i ﾘ:l　　 //　　　　 　 　 、
+　 　 |　　　　 　 　 　 　 ｀ヾ.　＼ r‐’:ﾚ=‐' .ゝ...ノ＿＿＼＿))
+　　 弋　 　 　 　 　 　 　 　 ∨ソ`ー''`ー---一　‐─‐-､)¨´
+　　 　 } 、　　　　　　　　　　 i_..ノ _,　　　　ハ'
+　　 　 ∨＞､　iゝ.. 　 　 　 　 　 弋);;,ゞ..ノ　.′
+　　　　 ∨::::| 7!:::..ヾ.　　　　.′ヽ.　` 　 　 /
+　　　　　∨::|.′::::::ixxr, 　 /　　　:　　　　 I.
+　 　 　 　 }:::|..:ｉ..::::::|　i,,　　"'' ´ヾ.i 　 　 　 ﾊ
+　　　　 　 ::::|:::|::::::/　 }ヾ.　　　　ﾐゝ.　 ,、..:::::)
+　　　　 　 i:::|ヾ:::::i　 /.::::|｀ヾ..,,　..ノ＼ヾ..＿/
+　 　 　 　 |:::l　}:. {　 |::::..′　　　　　　 `ー''
+　 　 　 　 |:::| /.:/l　 !.:::l
+　　　　　 ﾉ..:ﾚ.:::ｉ::l　ﾉ..::|
+　　　 　 (人7.::::|:::V.::::::|
+　　　　　`''/ .:::::!ｰ:::::::::ﾊ
+　　　　　 /..::::::::| (_/＼__)
+　　　 　 (__/(,＿)
+*/
