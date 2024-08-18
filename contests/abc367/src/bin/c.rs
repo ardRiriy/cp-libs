94069bf2302d4b3bd7c33f494d88a6aca395316f
ardririy <hynnkgw27@gmail.com>
2024-08-18 16:31:48 +0900

    全部つくるタイプの辞書順の整数列生成はmulti_cartesian_productが便利

---
+++ contests/abc367/src/bin/c.rs
@@ -0,0 +1,78 @@
+use itertools::Itertools;
+use proconio::{input};
+
+fn solve1(n: usize, k: u32, r: &[u32]) {
+    let mut s = vec![1; n];
+
+    loop {
+        let sum = s.iter().sum::<u32>();
+        if sum % k == 0 {
+            println!("{}", s.iter().join(" "));
+        }
+
+        let mut i = n;
+
+        while i > 0 && s[i-1] == r[i-1] {
+            i -= 1;
+        }
+        if i == 0 {
+            break;
+        }
+
+        s[i-1] += 1;
+        for j in i..n {
+            s[j] = 1;
+        }
+    }
+}
+
+fn solve2(n: usize, k: u32, r: &[u32]) {
+    for ans in r.iter()
+        .map(|&x| 1..=x)
+        .multi_cartesian_product()
+        .filter(|v| v.iter().sum::<u32>() % k == 0)
+    {
+        println!("{}", ans.iter().join(" "));
+    }
+}
+
+fn main() {
+    input!{
+        n: usize,
+        k: u32,
+        r: [u32; n]
+    }
+    solve2(n, k, &r);
+}
+
+/*
+鹿のardririy, arDeeriy
+　　　　　　　　　　　　 　 　 　 　 　 　 ／
+　　　　　　　　　　　　　　　　　 　 　 //
+　　　　　　　　　 　 　 　 　 　 　 　 //　 　 　 　 |:
+　　　　　　　　　　　　　　　　　 　 // 　 　 　 　 .|i
+　　　　　　　　　 　 　 　 　 　 　 //　 　 　 　 　 ||
+　　　　　　　　　　　　　　　　　　l i　　　　　　 　 ||
+　　　　　.　´￣￣｀ｰ　、　　　　 | l　 　 　 　 　 ∥
+　　　 ／　 . 　 　 　_＞─- ､ 　 l |　　　　　　 .∥
+　　　i　 〆　 　 ／　 　 　 　 ＼.i | 　 　 　 　 ∥
+　　　| /　 　 ／ 　 　 　 　 　 　 l |　ﾊ　　　　.′
+　 　 ; :　 　 .′　　　　　　　　　 ヾゝi !　　　/.′
+　　 《　　　 :　　　 　 　 r─-､　　 i ﾘ:l　　 //　　　　 　 　 、
+　 　 |　　　　 　 　 　 　 ｀ヾ.　＼ r‐’:ﾚ=‐' .ゝ...ノ＿＿＼＿))
+　　 弋　 　 　 　 　 　 　 　 ∨ソ`ー''`ー---一　‐─‐-､)¨´
+　　 　 } 、　　　　　　　　　　 i_..ノ _,　　　　ハ'
+　　 　 ∨＞､　iゝ.. 　 　 　 　 　 弋);;,ゞ..ノ　.′
+　　　　 ∨::::| 7!:::..ヾ.　　　　.′ヽ.　` 　 　 /
+　　　　　∨::|.′::::::ixxr, 　 /　　　:　　　　 I.
+　 　 　 　 }:::|..:ｉ..::::::|　i,,　　"'' ´ヾ.i 　 　 　 ﾊ
+　　　　 　 ::::|:::|::::::/　 }ヾ.　　　　ﾐゝ.　 ,、..:::::)
+　　　　 　 i:::|ヾ:::::i　 /.::::|｀ヾ..,,　..ノ＼ヾ..＿/
+　 　 　 　 |:::l　}:. {　 |::::..′　　　　　　 `ー''
+　 　 　 　 |:::| /.:/l　 !.:::l
+　　　　　 ﾉ..:ﾚ.:::ｉ::l　ﾉ..::|
+　　　 　 (人7.::::|:::V.::::::|
+　　　　　`''/ .:::::!ｰ:::::::::ﾊ
+　　　　　 /..::::::::| (_/＼__)
+　　　 　 (__/(,＿)
+*/
