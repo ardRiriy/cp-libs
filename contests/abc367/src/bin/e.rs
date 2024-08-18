c51e3e959b767fbb336d09da7254a1883c153a33
ardririy <hynnkgw27@gmail.com>
2024-08-18 21:13:40 +0900

    ダブリングの練習問題

--- contests/abc367/src/bin/e.rs
+++ contests/abc367/src/bin/e.rs
@@ -8,43 +8,29 @@ fn main() {
         a: [u64 ; n]
     }
 
-    let mut visited = vec![false; n];
-    let mut res = vec![0; n];
+    // dubling[i][j] = 操作を2^i回行った後のj番目の要素のindex
+    let mut doubling = vec![vec![0; n]; 60];
+    doubling[0] = x.clone();
 
-    for s in 0..n {
-        if visited[s] {
-            continue;
-        }
-
-        let mut current = s;
-        let mut trace = vec![];
-        let mut indices = vec![0; n];
-
-        while !visited[current] {
-            visited[current] = true;
-            indices[current] = trace.len();
-            trace.push(current);
-            current = x[current];
-        }
-
-        let cycle_start = indices[current];
-        let cycle_length = trace.len() - cycle_start;
-        println!("{:?}", trace);
-        for i in 0..trace.len() {
-            let pos = if i < cycle_start {
-                if (k as usize) < i {
-                    trace[k as usize]
-                } else {
-                    trace[(k - i) % cycle_length]
-                }
-            } else {
-                trace[((k % cycle_length) + i) % cycle_length]
-            };
-            res[trace[i]] = a[pos];
+    for i in 1..60 {
+        for j in 0..n {
+            doubling[i][j] = doubling[i-1][doubling[i-1][j]];
         }
     }
+    let doubling = doubling;
 
-    println!("{}", res.iter().join(" "));
+    let mut ans = vec![0; n];
+    for i in 0..n {
+        let mut now = i;
+        for j in 0..60 {
+            if k >> j & 1 == 1 {
+                now = doubling[j][now];
+            }
+        }
+        ans[i] = a[now];
+    }
+
+    println!("{}", ans.iter().join(" "));
 }
 
 /*
