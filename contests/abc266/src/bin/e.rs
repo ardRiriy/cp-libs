c892902e096b628856810ea419893f3f8a1d399b
ardririy <hynnkgw27@gmail.com>
2024-09-17 20:29:51 +0900

    残りkターンの状態でiが出た時、k-1回振り直した時の期待値がiよりも大きいなら振り直した方が良い 再帰で解ける

---
+++ contests/abc266/src/bin/e.rs
@@ -0,0 +1,26 @@
+use proconio::{input};
+fn main() {
+    input!{
+        n: usize,
+    }
+    println!("{}", dfs(n));
+}
+
+fn dfs(turn: usize) -> f64 {
+    if turn == 0 {
+        return 0.;
+    }
+
+    let mut res = 0.;
+    let rec = dfs(turn-1);
+
+    for i in 1..=6 {
+        let p =  i as f64 / 6.;
+        if rec > i as f64 {
+            res += rec / 6.;
+        } else {
+            res += p;
+        }
+    }
+    res
+}
