ae2d7b40a12d758f9d5541b7539ce1ee95941ed8
ardririy <hynnkgw27@gmail.com>
2024-09-17 12:03:46 +0900

    頑張って言われた通りを実装する

---
+++ contests/abc256/src/bin/b.rs
@@ -0,0 +1,26 @@
+use proconio::{input};
+fn main() {
+    input!{
+        n: usize,
+        a: [usize; n]
+    }
+    let mut mass = vec![false; 4];
+    let mut res = 0;
+
+    for &x in a.iter() {
+        mass[0] = true;
+
+        for i in (0..4).rev() {
+            if !mass[i] {
+                continue;
+            }
+            mass[i] = false;
+            if i + x >= 4 {
+                res += 1;
+            } else {
+                mass[i+x] = true;
+            }
+        }
+    }
+    println!("{}", res);
+}
