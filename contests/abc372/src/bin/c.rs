4ad93a048495bfbf99f52e73fa2d5f1656740170
ardririy <hynnkgw27@gmail.com>
2024-09-21 23:02:35 +0900

    更新時に新たに「ABC」が作られる可能性がある場所は3通りかそれ以下なので、それを全探索すればOK

---
+++ contests/abc372/src/bin/c.rs
@@ -0,0 +1,59 @@
+use proconio::{input, marker::{Chars, Usize1}};
+fn main() {
+    input!{
+        n: usize,
+        q: usize,
+        mut s: Chars,
+    }
+
+    let mut ans = 0u64;
+    'i: for i in 0..n-2 {
+        for j in 0..3 {
+            if !(s[i + j] == ('A' as u8 + j as u8) as char) {
+                continue 'i;
+            }
+        }
+        ans += 1;
+    }
+
+
+    for _ in 0..q {
+        input! {
+            x: Usize1,
+            c: char,
+        }
+
+        let mut prev = 0;
+        let start = if x >= 2 { x - 2 } else { 0 };
+
+        'i: for i in start..=x {
+            for j in 0..3 {
+                if i + j >= n {
+                    break 'i;
+                }
+                if !(s[i + j] == ('A' as u8 + j as u8) as char) {
+                    continue 'i;
+                }
+            }
+            prev += 1;
+        }
+
+        s[x] = c;
+        let mut cur = 0;
+        'i: for i in start..=x {
+            for j in 0..3 {
+                if i + j >= n {
+                    break 'i;
+                }
+                if !(s[i + j] == ('A' as u8 + j as u8) as char) {
+                    continue 'i;
+                }
+            }
+            cur += 1;
+        }
+
+        ans = ans + cur - prev;
+        println!("{}", ans);
+    }
+
+}
