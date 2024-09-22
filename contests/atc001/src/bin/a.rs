7c09f2db2d5864c9565413395b4cb774d327a966
ardririy <hynnkgw27@gmail.com>
2024-09-22 20:56:43 +0900

    深さ優先探索の実装

---
+++ contests/atc001/src/bin/a.rs
@@ -0,0 +1,32 @@
+use cps::consts::{DI, DJ};
+use proconio::{input, marker::Chars};
+fn main() {
+    input!{
+        h: usize,
+        w: usize,
+        mut c: [Chars; h],
+    }
+
+    let pos = |chr: char| -> (usize, usize) {
+        let res = c.iter().flatten().position(|elm| *elm == chr).unwrap();
+        return (res / w, res % w);
+    };
+
+    let mut stk = vec![pos('s')];
+    while let Some((pi, pj)) = stk.pop() {
+        for r in 0..4 {
+            let ni = pi.wrapping_add(DI[r]);
+            let nj = pj.wrapping_add(DJ[r]);
+            if ni < h && nj < w {
+                if c[ni][nj] == 'g' {
+                    println!("Yes");
+                    return;
+                } else if c[ni][nj] == '.' {
+                    c[ni][nj] = '_';
+                    stk.push((ni, nj));
+                }
+            }
+        }
+    }
+    println!("No");
+}
