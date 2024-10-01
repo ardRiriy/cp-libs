2996da72b55266726f7f25674c74ae22cf5d4fa7
ardririy <hynnkgw27@gmail.com>
2024-10-02 00:03:28 +0900

    ナイーブなDPを高速化するところから始める

--- contests/abc370/src/bin/e.rs
+++ contests/abc370/src/bin/e.rs
@@ -1,7 +1,31 @@
+use std::collections::BTreeMap;
+
+use ac_library::ModInt998244353;
 use proconio::{input};
 fn main() {
     input!{
+        n: usize,
+        k: i64,
+        a: [i64; n],
+    }
 
+    let mut map = BTreeMap::new();
+    map.insert(0, ModInt998244353::new(1));
+    let mut all = ModInt998244353::new(1);
+    let mut acc = 0;
+
+    for (i, x) in a.iter().enumerate() {
+        acc += *x;
+        let t = match map.get(&(acc-k)) {
+            Some(x) => *x,
+            None => ModInt998244353::new(0),
+        };
+        let current = all - t;
+        *map.entry(acc).or_insert(ModInt998244353::new(0)) += current;
+        all += current;
+
+        if i == n-1 {
+            println!("{}", current);
+        }
     }
 }
-
