4ed5f33e38cda768fe9f8ededd574f22f3e3c674
ardririy <hynnkgw27@gmail.com>
2024-10-14 14:14:40 +0900

    中央に着目して、左右に文字cが何個あるかを数える 累積和

--- contests/abc375/src/bin/d.rs
+++ contests/abc375/src/bin/d.rs
@@ -1,19 +1,17 @@
+use cps::cumulative_sum::CumulativeSum;
 use proconio::{input, marker::Chars};
-fn main() {
-    input!{
-        s: Chars,
-    }
 
+fn to_usize(c: char) -> usize {
+    c as usize - 'A' as usize
+}
+
+fn solve1(s: &[char]) {
+    // 左から順に見る
     let mut cnt = vec![0u64; 26];
     let mut adder = vec![0u64; 26];
-    let mut last = vec![1usize << 60; 26];
 
-
-    fn to_usize(c: char) -> usize {
-        c as usize - 'A' as usize
-    }
     let mut ans = 0;
-    for (i, &ci) in s.iter().enumerate() {
+    for &ci in s.iter() {
         for cj in 'A'..='Z' {
             if ci == cj {
                 ans += adder[to_usize(ci)];
@@ -27,4 +25,38 @@ fn main() {
     }
 
     println!("{ans}");
+}
+
+fn solve2(s: &[char]) {
+    // 中央に着目
+    let mut csums :Vec<CumulativeSum<u64>> = vec![];
+
+    for c in 'A'..='Z' {
+        let v = s.iter()
+            .map(|si| if *si == c { 1 } else { 0 })
+            .collect();
+        csums.push(CumulativeSum::new(&v));
+    }
+
+
+    let mut ans = 0;
+    for i in 0..s.len() {
+        if i != 0 && i != s.len() {
+            for c in 'A'..='Z' {
+                let l = csums[to_usize(c)].get(..i);
+                let r = csums[to_usize(c)].get(i+1..);
+                ans += l * r;
+            }
+        }
+    }
+    println!("{ans}");
+}
+
+fn main() {
+    input!{
+        s: Chars,
+    }
+
+    // solve1(&s);
+    solve2(&s);
 }
