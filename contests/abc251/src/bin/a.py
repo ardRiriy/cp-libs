641aae8a2b782f156db707975493bebc79f2696f
ardRiriy <hynnkgw27@gmail.com>
2024-10-15 23:48:53 +0900

    全部調べる

---
+++ contests/abc251/src/bin/a.py
@@ -0,0 +1,7 @@
+# r, x, y = map(int, input().split()) # r, x, yのように横並びのものを受け取るとき
+# a = list(map(int, input().split())) # a_0, a_1, ..., a_n を受け取るとき
+a = input()
+ans = ""
+while len(ans) < 6:
+    ans += a
+print(ans)
