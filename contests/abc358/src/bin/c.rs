2e7e4212246c6c04cad904b6577d48e39bb33e54
ardririy <hynnkgw27@gmail.com>
2024-11-20 21:00:20 +0900

    fix warnings

--- contests/abc358/src/bin/c.rs
+++ contests/abc358/src/bin/c.rs
@@ -1,14 +1,13 @@
-use std::io::BufRead;
-use std::str::{self, FromStr};
-
 use itertools::Itertools;
+use proconio::input;
+use proconio::marker::Chars;
 
-fn solve(rdr: &mut StdinReader<impl BufRead>) {
-    let n: usize = rdr.r();
-    let m: usize = rdr.r();
-    let tmp: Vec<String> = rdr.rv(n);
-
-    let ss = tmp.iter().map(|s| s.chars().collect_vec()).collect_vec();
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        ss: [Chars; n],
+    }
 
     for i in 1..=n {
         for v in (0..n).combinations(i) {
@@ -27,113 +26,4 @@ fn solve(rdr: &mut StdinReader<impl BufRead>) {
             }
         } 
     }
-
 }
-
-/*
-
-            ▄▌▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
-     ▄▄██▌█            宅急便です！
-▄▄▄▌▐██▌█ Rating +25 :) をお届けに参りました！
-███████▌█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
-▀(⊙)▀▀▀▀(⊙)(⊙)▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀(⊙
-
-*/
-
-static INF: u64 = 1e18 as u64;
-
-trait ChLibs<T: std::cmp::Ord> {
-    fn chmin(&mut self, elm: T) -> bool;
-    fn chmax(&mut self, elm: T) -> bool;
-}
-
-impl<T: std::cmp::Ord> ChLibs<T> for T {
-    fn chmin(&mut self, elm: T) -> bool {
-        if *self > elm {
-            *self = elm;
-            true
-        } else {
-            false
-        }
-    }
-
-    fn chmax(&mut self, elm: T) -> bool {
-        if *self < elm {
-            *self = elm;
-            true
-        } else {
-            false
-        }
-    }
-}
-
-pub struct StdinReader<R: BufRead> {
-    reader: R,
-    buf: Vec<u8>,
-    pos: usize,
-}
-
-impl<R: BufRead> StdinReader<R> {
-    fn new(reader: R) -> Self {
-        Self {
-            reader,
-            buf: Vec::new(),
-            pos: 0,
-        }
-    }
-
-    fn r<T: FromStr>(&mut self) -> T {
-        self._read_until(&[b' ', b'\n'])
-    }
-
-    fn rv<T: FromStr>(&mut self, n: usize) -> Vec<T> {
-        let mut v: Vec<T> = Vec::with_capacity(n);
-        for _ in 0..n {
-            v.push(self.r());
-        }
-        v
-    }
-
-    fn _read_until<T: FromStr>(&mut self, delims: &[u8]) -> T {
-        loop {
-            if self.pos < self.buf.len() {
-                let start = self.pos;
-                while self.pos < self.buf.len() && !delims.contains(&self.buf[self.pos]) {
-                    self.pos += 1;
-                }
-
-                if self.pos < self.buf.len() {
-                    let end = self.pos;
-                    self.pos += 1;
-                    let token: &[u8] = &self.buf[start..end];
-                    let elem: &str = unsafe { str::from_utf8_unchecked(token) };
-
-                    return elem
-                        .parse()
-                        .unwrap_or_else(|_| panic!("{}", format!("failed parsing: {}", elem)));
-                }
-            }
-
-            self.buf.clear();
-            self.pos = 0;
-            let len = self
-                .reader
-                .read_until(b'\n', &mut self.buf)
-                .expect("failed reading bytes");
-            if len == 0 {
-                panic!("early eof");
-            }
-        }
-    }
-}
-
-fn main() {
-    // input! { mut i: usize }
-    let mut i = 1;
-    let mut reader = StdinReader::new(std::io::stdin().lock());
-    while i != 0 {
-        solve(&mut reader);
-        i -= 1;
-    }
-}
-
--- contests/abc358/src/bin/d.rs
+++ contests/abc358/src/bin/d.rs
@@ -1,11 +1,12 @@
-use std::io::BufRead;
-use std::str::{self, FromStr};
+use proconio::input;
 
-fn solve(rdr: &mut StdinReader<impl BufRead>) {
-    let n: usize = rdr.r();
-    let m: usize = rdr.r();
-    let mut a: Vec<u64> = rdr.rv(n);
-    let mut b: Vec<u64> = rdr.rv(m);
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        mut a: [u64; n],
+        mut b: [u64; n]
+    }
 
     a.sort();
     b.sort();
@@ -29,111 +30,3 @@ fn solve(rdr: &mut StdinReader<impl BufRead>) {
     }
     println!("{}", ans);
 }
-
-/*
-
-            ▄▌▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
-     ▄▄██▌█            宅急便です！
-▄▄▄▌▐██▌█ Rating +25 :) をお届けに参りました！
-███████▌█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
-▀(⊙)▀▀▀▀(⊙)(⊙)▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀(⊙
-
-*/
-
-static INF: u64 = 1e18 as u64;
-
-trait ChLibs<T: std::cmp::Ord> {
-    fn chmin(&mut self, elm: T) -> bool;
-    fn chmax(&mut self, elm: T) -> bool;
-}
-
-impl<T: std::cmp::Ord> ChLibs<T> for T {
-    fn chmin(&mut self, elm: T) -> bool {
-        if *self > elm {
-            *self = elm;
-            true
-        } else {
-            false
-        }
-    }
-
-    fn chmax(&mut self, elm: T) -> bool {
-        if *self < elm {
-            *self = elm;
-            true
-        } else {
-            false
-        }
-    }
-}
-
-pub struct StdinReader<R: BufRead> {
-    reader: R,
-    buf: Vec<u8>,
-    pos: usize,
-}
-
-impl<R: BufRead> StdinReader<R> {
-    fn new(reader: R) -> Self {
-        Self {
-            reader,
-            buf: Vec::new(),
-            pos: 0,
-        }
-    }
-
-    fn r<T: FromStr>(&mut self) -> T {
-        self._read_until(&[b' ', b'\n'])
-    }
-
-    fn rv<T: FromStr>(&mut self, n: usize) -> Vec<T> {
-        let mut v: Vec<T> = Vec::with_capacity(n);
-        for _ in 0..n {
-            v.push(self.r());
-        }
-        v
-    }
-
-    fn _read_until<T: FromStr>(&mut self, delims: &[u8]) -> T {
-        loop {
-            if self.pos < self.buf.len() {
-                let start = self.pos;
-                while self.pos < self.buf.len() && !delims.contains(&self.buf[self.pos]) {
-                    self.pos += 1;
-                }
-
-                if self.pos < self.buf.len() {
-                    let end = self.pos;
-                    self.pos += 1;
-                    let token: &[u8] = &self.buf[start..end];
-                    let elem: &str = unsafe { str::from_utf8_unchecked(token) };
-
-                    return elem
-                        .parse()
-                        .unwrap_or_else(|_| panic!("{}", format!("failed parsing: {}", elem)));
-                }
-            }
-
-            self.buf.clear();
-            self.pos = 0;
-            let len = self
-                .reader
-                .read_until(b'\n', &mut self.buf)
-                .expect("failed reading bytes");
-            if len == 0 {
-                panic!("early eof");
-            }
-        }
-    }
-}
-
-fn main() {
-    // input! { mut i: usize }
-    let mut i = 1;
-    let mut reader = StdinReader::new(std::io::stdin().lock());
-    while i != 0 {
-        solve(&mut reader);
-        i -= 1;
-    }
-}
-
