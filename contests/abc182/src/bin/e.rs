a804c5543d4a7b9b076d40cc66343439b4cebfbf
ardririy <hynnkgw27@gmail.com>
2024-12-23 22:10:51 +0900

    列、行ごとに壁の位置と明かりの位置をsetで持ち、マスごとに「そのマスを照らす明かりがあるか？」を判定する

--- contests/abc182/src/bin/e.rs
+++ contests/abc182/src/bin/e.rs
@@ -1,51 +1,87 @@
-use proconio::input;
+use std::collections::BTreeSet;
 
-fn solve() {
-    input! {
-    }
-
-}
-
-/*
-
-            ▄▌▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
-     ▄▄██▌█            宅急便です！
-▄▄▄▌▐██▌█ Rating +25 :) をお届けに参りました！
-███████▌█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
-▀(⊙)▀▀▀▀(⊙)(⊙)▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀(⊙
-
-*/
-
-
-static INF: u64 = 1e18 as u64;
-
-trait ChLibs<T: std::cmp::Ord> {
-    fn chmin(&mut self, elm: T) -> bool;
-    fn chmax(&mut self, elm: T) -> bool;
-}
-
-impl<T: std::cmp::Ord> ChLibs<T> for T {
-    fn chmin(&mut self, elm: T) -> bool {
-        if *self > elm {
-                *self = elm;
-                true
-            } else { false }
-    }
-
-    fn chmax(&mut self, elm: T) -> bool {
-        if *self < elm {
-                *self = elm;
-                true
-            } else { false }
-    }
-}
-
+use itertools::iproduct;
+use proconio::{input, marker::Usize1};
 
 fn main() {
-    // input! { i: usize }
-    let mut i = 1;
-    while i != 0 {
-        solve();
-        i -= 1;
+    input! {
+        h: usize,
+        w: usize,
+        n: usize,
+        m: usize,
+        akari: [(Usize1, Usize1); n],
+        block: [(Usize1, Usize1); m],
     }
+
+    let mut akari_row = vec![BTreeSet::new(); h];
+    let mut akari_col = vec![BTreeSet::new(); w];
+    for &(i, j) in &akari {
+        akari_row[i].insert(j);
+        akari_col[j].insert(i);
+    }
+
+    let mut blcok_row = vec![BTreeSet::new(); h];
+    let mut block_col = vec![BTreeSet::new(); w];
+    for &(i, j) in &block {
+        blcok_row[i].insert(j);
+        block_col[j].insert(i);
+    }
+
+    let mut ans = 0i64;
+    let blocks = block.iter().copied().collect::<BTreeSet<_>>();
+
+    'i_j: for (i, j) in iproduct!(0..h, 0..w) {
+        if blocks.contains(&(i, j)) {
+            continue;
+        }
+
+        if let Some(&akari_j) = akari_row[i].range(0..=j).last() {
+            if let Some(&block_j) = blcok_row[i].range(0..=j).last() {
+                if akari_j > block_j {
+                    ans += 1;
+                    continue 'i_j;
+                }
+            } else {
+                ans += 1;
+                continue 'i_j;
+            }
+        }
+
+        if let Some(&akari_j) = akari_row[i].range(j..).next() {
+            if let Some(&block_j) = blcok_row[i].range(j..).next() {
+                if akari_j < block_j {
+                    ans += 1;
+                    continue 'i_j;
+                }
+            } else {
+                ans += 1;
+                continue 'i_j;
+            }
+        }
+
+        if let Some(&akari_i) = akari_col[j].range(0..=i).last() {
+            if let Some(&block_i) = block_col[j].range(0..=i).last() {
+                if akari_i > block_i {
+                    ans += 1;
+                    continue 'i_j;
+                }
+            } else {
+                ans += 1;
+                continue 'i_j;
+            }
+        }
+
+        if let Some(&akari_i) = akari_col[j].range(i..).next() {
+            if let Some(&block_i) = block_col[j].range(i..).next() {
+                if akari_i < block_i {
+                    ans += 1;
+                    continue 'i_j;
+                }
+            } else {
+                ans += 1;
+                continue 'i_j;
+            }
+        }
+    }
+    println!("{ans}");
 }
