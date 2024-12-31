9d9ee01e74c669cdfe5fa322d640deecf749b183
ardRiriy <hynnkgw27@gmail.com>
2024-12-31 19:26:21 +0900

    python関連を整備した

--- util/ac_functions.sh
+++ util/ac_functions.sh
@@ -3,27 +3,31 @@
 acsub() {
     echo "usage: acsub <language> <problem> <url>"
     rm -rf test
-    oj d $3
+    uv run oj d $3
     if [ $1 = "rs" ]; then
-        oj t -c "cargo run --features local --bin $2" || return 1
-        python3 ../../util/file_merger.py $2
-        oj s $3 src/bin/$2.rs -y
-        python3 ../../util/after_submission.py $2
+        uv run oj t -c "cargo run --features local --bin $2" || return 1
+        uv run python3 ../../util/file_merger.py $2
+        uv run oj s $3 src/bin/$2.rs -y
+        uv run python3 ../../util/after_submission.py $2
     elif [ $1 = "py" ]; then
-        oj t -c "python3 src/bin/$2.py" || return 1
+        uv run oj t -c "uv run python3 src/bin/$2.py" || return 1
         # pypyで提出
-        oj s $3 src/bin/$2.py -l 5078
+        uv run oj s $3 src/bin/$2.py -l 5078
     else
         echo "language $1 is not exist!"
     fi
 }
 
 actest() {
-    roj test -p $1 -l $2
+    echo "usage: acsub <language> <problem> <url>"
+    rm -rf test
+    uv run oj d $3
+    if [ $1 = "rs" ]; then
+        uv run oj t -c "cargo run --features local --bin $2" || return 1
+    elif [ $1 = "py" ]; then
+        uv run oj t -c "uv run python3 src/bin/$2.py" || return 1
+    else
+        echo "language $1 is not exist!"
+    fi
 }
 
-acinit(){
-    roj init -c $1
-    cd $1
-    code .
-}
