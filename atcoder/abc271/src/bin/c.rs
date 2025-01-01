e9607f550c49bdb6353af272b4a352862a414ba5
ardRiriy <hynnkgw27@gmail.com>
2025-01-02 02:09:53 +0900

    前から貪欲

--- atcoder/abc271/src/bin/c.rs
+++ atcoder/abc271/src/bin/c.rs
@@ -1,53 +1,52 @@
-use proconio::{input};
-fn solve() {
-    input!{
-        
-    }
-}
+use std::convert::TryInto;
 
-/*
-
-            ▄▌▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
-     ▄▄██▌█            宅急便です！
-▄▄▄▌▐██▌█ Rating +25 :) をお届けに参りました！
-███████▌█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
-▀(⊙)▀▀▀▀(⊙)(⊙)▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀(⊙
-
-*/
-
-static INF: u64 = 1e18 as u64;
-
-trait ChLibs<T: std::cmp::Ord> {
-    fn chmin(&mut self, elm: T) -> bool;
-    fn chmax(&mut self, elm: T) -> bool;
-}
-
-impl<T: std::cmp::Ord> ChLibs<T> for T {
-    fn chmin(&mut self, elm: T) -> bool {
-        if *self > elm {
-            *self = elm;
-            true
-        } else {
-            false
-        }
-    }
-
-    fn chmax(&mut self, elm: T) -> bool {
-        if *self < elm {
-            *self = elm;
-            true
-        } else {
-            false
-        }
-    }
-}
+use cps::consts::INF;
+use cps::multiset::MultiSet;
+use proconio::input;
 
 fn main() {
-    // input! { mut i: usize }
-    let mut i = 1;
-    while i != 0 {
-        solve();
-        i -= 1;
+    input! {
+        n: usize,
+        a: [usize; n],
     }
-}
 
+    const N: usize = 3e5 as usize + 1;
+    let mut v = vec![false; N];
+    let mut mset = MultiSet::new();
+    for ai in a {
+        if ai >= N {
+            mset.add(INF, 1);
+        } else if v[ai] {
+            mset.add(INF, 1);
+        } else {
+            v[ai] = true;
+            mset.add(ai.try_into().unwrap(), 1);
+        }
+    }
+
+    let mut ans = 0;
+    for i in 1.. {
+        if let Some(val) = mset.get(i) {
+            assert_eq!(val, 1);
+            mset.remove(i, 1);
+        } else {
+            if let Some((&k, &v)) = mset.max_key() {
+                if v >= 2 {
+                    mset.remove(k, 2);
+                } else {
+                    assert_eq!(v, 1);
+                    mset.remove(k, 1);
+                    if let Some((&l, _)) = mset.max_key() {
+                        mset.remove(l, 1);
+                    } else {
+                        break;
+                    }
+                }
+            } else {
+                break;
+            }
+        }
+        ans = i;
+    }
+    println!("{}", ans);
+}
