a65614930379fcf6538b507361be50fd7e2c330c
ardRiriy <hynnkgw27@gmail.com>
2025-01-03 20:00:59 +0900

    解説AC x, yを分離して座標系から思考を外す
    sum(|x-xi|), sum(|y-yi|)をそれぞれあり得る全てのx, yについて前計算する。
    
    すると、「あるx座標に対して条件を満たすy座標」がいくつ存在するか？という問題に帰着できて、
    この問題は二分探索/尺取りで可能

--- atcoder/abc366/src/bin/e.rs
+++ atcoder/abc366/src/bin/e.rs
@@ -1,94 +1,50 @@
-use ac_library::{LazySegtree, MapMonoid, Monoid};
+use cps::veclibs::VecLibs;
+use itertools::Itertools;
 use proconio::input;
 
-const INF :i64 = 1 << 60;
-
-struct SumMax;
-impl Monoid for SumMax {
-    type S = i64;
-
-    fn identity() -> Self::S {
-        -INF
-    }
-
-    fn binary_operation(a: &Self::S, b: &Self::S) -> Self::S {
-        *a + *b
-    }
-}
-
-struct AffineMax;
-impl MapMonoid for AffineMax {
-    type M = SumMax;
-    type F = i64;
-
-    fn identity_map() -> Self::F {
-        0
-    }
-
-    fn mapping(f: &Self::F, x: &<Self::M as Monoid>::S) -> <Self::M as Monoid>::S {
-        f + x
-    }
-
-    fn composition(f: &Self::F, g: &Self::F) -> Self::F {
-        f + g
-    }
-}
-
-
 fn main() {
-    input!{
+    input! {
         n: usize,
         d: i64,
-        point: [(i64, i64); n]
+        p: [(i64, i64); n],
     }
-    const MAX :usize = 3e6 as usize + 1;
-    const BASE :i64 = 1e6 as i64;
 
-    let mut seg1 :LazySegtree<AffineMax> = LazySegtree::new(MAX);
-    let mut seg2 :LazySegtree<AffineMax> = LazySegtree::new(MAX);
+    static M: i64 = 1e6 as i64; 
 
-    for &(x, y) in point.iter() {
-        let i = (x + BASE) as usize;
-        dbg!(i);
-        if y >= 0 {
-            let val = seg1.get(i);
-            if val == -INF {
-                seg1.set(i, i as i64 + y);
-            } else {
-                seg1.set(i, val + i as i64 + y);
-            }
-        } else {
-            let val = seg2.get(i);
-            if val == -INF {
-                seg2.set(i, i as i64 - y);
-            } else {
-                seg2.set(i, val + i as i64 - y);
-            }
+    let xs = p.iter().map(|&(x, _)| x).sorted().collect_vec();
+    let ys = p.iter().map(|&(_, y)| y).sorted().collect_vec();
+
+    let mut xmap = vec![];
+    let mut ymap = vec![];
+
+    let mut x_i = 0;
+    let mut y_i = 0;
+
+    let mut f_x = xs.iter().map(|&x| (-(M+d) - x).abs()).sum::<i64>(); 
+    let mut g_y = ys.iter().map(|&y| (-(M+d) - y).abs()).sum::<i64>(); 
+
+    for i in -(M+d)..=M+d {
+        xmap.push(f_x);
+        ymap.push(g_y);
+
+        f_x += x_i as i64;
+        f_x -= (n - x_i) as i64;
+        g_y += y_i as i64;
+        g_y -= (n - y_i) as i64;
+
+        while x_i < n && xs[x_i] == i+1 {
+            x_i += 1;
+        }
+        while y_i < n && ys[y_i] == i+1 {
+            y_i += 1;
         }
     }
 
+    ymap.sort_unstable();
     let mut ans = 0;
-
-    for i in 0..MAX {
-        // 下にどれぐらい伸ばせるか？
-        let val = seg1.all_prod();
-        if val >= 0 && d >= val {
-            ans += d - val;
-            dbg!(i, ans, val);
-        } 
-
-        // 上にどれぐらい伸ばせるか
-        let val = d - seg2.all_prod();
-        if val >= 0 && d >= val {
-            ans += d - val;
-            dbg!(i, ans, val);
-        }
-
-        seg1.apply_range(0..=i, 1);
-        seg1.apply_range(i.., -1);
-
-        seg2.apply_range(0..=i, 1);
-        seg2.apply_range(i.., -1);
+    for xi in xmap {
+        let p = ymap.lower_bound(d-xi+1);
+        ans += p;
     }
-    println!("{ans}")
+    println!("{}", ans);
 }
