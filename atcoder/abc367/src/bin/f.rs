1ee688e906dae52159c78ccc4ff8ec34bb9ea455
ardRiriy <hynnkgw27@gmail.com>
2025-01-04 17:43:10 +0900

    解説AC: 整数の多重集合の判定にはZobrist Hashが使える

--- atcoder/abc367/src/bin/f.rs
+++ atcoder/abc367/src/bin/f.rs
@@ -1,38 +1,37 @@
-use proconio::{input};
+use cps::zobrist_hash::ZobristHash;
+use proconio::input;
 fn main() {
     input!{
+        n: usize,
+        q: usize,
+        a: [u64; n],
+        b: [u64; n],
+    }
+    let mut zh = ZobristHash::new();
 
+    let mut a_sum = vec![0];
+    for (i, &ai) in a.iter().enumerate() {
+        let hash = zh.get(ai);
+        let val = a_sum[i] + hash;
+        a_sum.push(val);
+    }
+
+    let mut b_sum = vec![0];
+    for (i, &bi) in b.iter().enumerate() {
+        let hash = zh.get(bi);
+        let val = b_sum[i] + hash;
+        b_sum.push(val);
+    }
+
+    for _ in 0..q {
+        input! {
+            l: usize,
+            r: usize,
+            s: usize,
+            t: usize,
+        }
+        let x = a_sum[r] - a_sum[l-1];
+        let y = b_sum[t] - b_sum[s-1];
+        println!("{}", if x == y && r - l == t - s { "Yes" } else { "No" });
     }
 }
-
-/*
-鹿のardririy, arDeeriy
-　　　　　　　　　　　　 　 　 　 　 　 　 ／
-　　　　　　　　　　　　　　　　　 　 　 //
-　　　　　　　　　 　 　 　 　 　 　 　 //　 　 　 　 |:
-　　　　　　　　　　　　　　　　　 　 // 　 　 　 　 .|i
-　　　　　　　　　 　 　 　 　 　 　 //　 　 　 　 　 ||
-　　　　　　　　　　　　　　　　　　l i　　　　　　 　 ||
-　　　　　.　´￣￣｀ｰ　、　　　　 | l　 　 　 　 　 ∥
-　　　 ／　 . 　 　 　_＞─- ､ 　 l |　　　　　　 .∥
-　　　i　 〆　 　 ／　 　 　 　 ＼.i | 　 　 　 　 ∥
-　　　| /　 　 ／ 　 　 　 　 　 　 l |　ﾊ　　　　.′
-　 　 ; :　 　 .′　　　　　　　　　 ヾゝi !　　　/.′
-　　 《　　　 :　　　 　 　 r─-､　　 i ﾘ:l　　 //　　　　 　 　 、
-　 　 |　　　　 　 　 　 　 ｀ヾ.　＼ r‐’:ﾚ=‐' .ゝ...ノ＿＿＼＿))
-　　 弋　 　 　 　 　 　 　 　 ∨ソ`ー''`ー---一　‐─‐-､)¨´
-　　 　 } 、　　　　　　　　　　 i_..ノ _,　　　　ハ'
-　　 　 ∨＞､　iゝ.. 　 　 　 　 　 弋);;,ゞ..ノ　.′
-　　　　 ∨::::| 7!:::..ヾ.　　　　.′ヽ.　` 　 　 /
-　　　　　∨::|.′::::::ixxr, 　 /　　　:　　　　 I.
-　 　 　 　 }:::|..:ｉ..::::::|　i,,　　"'' ´ヾ.i 　 　 　 ﾊ
-　　　　 　 ::::|:::|::::::/　 }ヾ.　　　　ﾐゝ.　 ,、..:::::)
-　　　　 　 i:::|ヾ:::::i　 /.::::|｀ヾ..,,　..ノ＼ヾ..＿/
-　 　 　 　 |:::l　}:. {　 |::::..′　　　　　　 `ー''
-　 　 　 　 |:::| /.:/l　 !.:::l
-　　　　　 ﾉ..:ﾚ.:::ｉ::l　ﾉ..::|
-　　　 　 (人7.::::|:::V.::::::|
-　　　　　`''/ .:::::!ｰ:::::::::ﾊ
-　　　　　 /..::::::::| (_/＼__)
-　　　 　 (__/(,＿)
-*/
