53dc1930985bddc02de925b5898cc2bb5485a022
ardRiriy <hynnkgw27@gmail.com>
2025-01-06 17:23:04 +0900

    multisetにイテレータを実装

--- cps/src/multiset.rs
+++ cps/src/multiset.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct MultiSet<T, U> {
     map: std::collections::BTreeMap<T, U>,
 }
@@ -47,5 +47,40 @@ where T: Ord + Copy, U: num::PrimInt + std::ops::AddAssign
 
     pub fn len(&self) -> usize {
         self.map.len()
+    }
+
+    pub fn iter(&self) -> std::collections::btree_map::Iter<T, U> {
+        self.map.iter()
+    }
+
+    pub fn iter_mut(&mut self) -> std::collections::btree_map::IterMut<T, U> {
+        self.map.iter_mut()
+    }
+}
+
+impl<T, U> IntoIterator for MultiSet<T, U> {
+    type Item = (T, U);
+    type IntoIter = std::collections::btree_map::IntoIter<T, U>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.map.into_iter()
+    }
+}
+
+impl<'a, T, U> IntoIterator for &'a MultiSet<T, U> {
+    type Item = (&'a T, &'a U);
+    type IntoIter = std::collections::btree_map::Iter<'a, T, U>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.map.iter()
+    }
+}
+
+impl<'a, T, U> IntoIterator for &'a mut MultiSet<T, U> {
+    type Item = (&'a T, &'a mut U);
+    type IntoIter = std::collections::btree_map::IterMut<'a, T, U>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.map.iter_mut()
     }
 }
