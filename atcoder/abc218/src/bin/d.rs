a42a81624ee121b65f7f2699559f0789b4b985f0
ardRiriy <hynnkgw27@gmail.com>
2025-01-09 22:32:38 +0900

    x座標が同じものについてy座標の組を取って、その組数でsumを取る

--- atcoder/abc218/src/bin/d.rs
+++ atcoder/abc218/src/bin/d.rs
@@ -1,53 +1,30 @@
-use proconio::{input};
-fn solve() {
-    input!{
-        
-    }
-}
+use std::collections::BTreeMap;
 
-/*
-
-            ▄▌▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
-     ▄▄██▌█            宅急便です！
-▄▄▄▌▐██▌█ Rating +25 :) をお届けに参りました！
-███████▌█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
-▀(⊙)▀▀▀▀(⊙)(⊙)▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀(⊙
-
-*/
-
-static INF: u64 = 1e18 as u64;
-
-trait ChLibs<T: std::cmp::Ord> {
-    fn chmin(&mut self, elm: T) -> bool;
-    fn chmax(&mut self, elm: T) -> bool;
-}
-
-impl<T: std::cmp::Ord> ChLibs<T> for T {
-    fn chmin(&mut self, elm: T) -> bool {
-        if *self > elm {
-            *self = elm;
-            true
-        } else {
-            false
-        }
-    }
-
-    fn chmax(&mut self, elm: T) -> bool {
-        if *self < elm {
-            *self = elm;
-            true
-        } else {
-            false
-        }
-    }
-}
-
+use itertools::Itertools;
+use proconio::input;
 fn main() {
-    // input! { mut i: usize }
-    let mut i = 1;
-    while i != 0 {
-        solve();
-        i -= 1;
+    input!{
+        n: usize,
+        p: [(u64, u64); n],
     }
+
+    let mut map = BTreeMap::new();
+    for (&(ax, ay), &(bx, by)) in p.iter().tuple_combinations() {
+        if ax == bx {
+            let a = ay.min(by);
+            let b = ay.max(by);
+            *map.entry((a, b)).or_insert(0) += 1u64;
+        }
+    }
+
+    fn sum(n: u64) -> u64 {
+        n * (n+1) / 2
+    }
+    
+    let mut ans = 0;
+    for (_, val) in map {
+        ans += sum(val-1);
+    }
+    println!("{}", ans);
 }
 
