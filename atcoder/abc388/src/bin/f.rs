1e496abe235bcc291ff710daadf4d3079a8ec9fe
ardRiriy <hynnkgw27@gmail.com>
2025-01-12 15:31:42 +0900

    contest init

---
+++ atcoder/abc388/src/bin/f.rs
@@ -0,0 +1,14 @@
+#[allow(unused_imports)]
+use cps::debug::*;
+use proconio::input;
+
+fn main() {
+    input!{
+        n: usize,
+        m: usize,
+        a: usize,
+        b: usize,
+        mut w: [(u64, u64); m],
+    }
+}
+
