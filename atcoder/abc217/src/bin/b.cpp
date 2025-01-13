c9bacee73be344bdb6da3e416db4999da70992a8
ardRiriy <hynnkgw27@gmail.com>
2025-01-13 23:39:57 +0900

    .

---
+++ atcoder/abc217/src/bin/b.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(v) v.begin(),v.end()
+#define resort(v) sort(v.rbegin(),v.rend())
+using ll = long long;
+using ull = unsigned long long;
+using vll=vector<ll>;
+using vvll = vector<vector<ll>>;
+using P = pair<ll,ll>;
+using vp=vector<pair<ll, ll>>;
+
+const ll inf=1ll<<60;
+#define mod10 (ll)1e9+7
+#define mod99 (ll)998244353
+const double PI = acos(-1);
+
+#define rep(i,n) for (ll i=0;i<n;++i)
+#define per(i,n) for(ll i=n-1;i>=0;--i)
+#define rep2(i,a,n) for (ll i=a;i<n;++i)
+#define per2(i,a,n) for (ll i=n-1;i>=a;--i)
+
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return true; } return false; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return true; } return false; }
+
+ll dx[] = {1, 0, -1, 0, -1, 1, -1, 1};
+ll dy[] = {0, 1, 0, -1, -1, 1, 1, -1};
+
+void solve() {
+    set<string> a;
+    a.insert("ABC");
+    a.insert("ARC");
+    a.insert("AGC");
+    a.insert("AHC");
+    rep(i, 3) {
+        string s;
+        cin >> s;
+        a.erase(s);
+    }
+    cout << *a.begin() << "\n";
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int t = 1;
+    // cin >> t;
+    while(t--)solve();
+}
