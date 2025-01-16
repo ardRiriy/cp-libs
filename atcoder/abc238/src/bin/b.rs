da7c03976b39aa3d6e756b294f2b700785c3d9af
ardRiriy <hynnkgw27@gmail.com>
2025-01-16 23:51:37 +0900

    360を剰余として切れ込みの場所を見つける 0と360も切れていることにして、tuple_windowsで全探索

--- atcoder/abc238/src/bin/b.rs
+++ atcoder/abc238/src/bin/b.rs
@@ -1,53 +1,19 @@
-use proconio::{input};
-fn solve() {
-    input!{
-        
+use itertools::Itertools;
+use proconio::input;
+
+fn main(){
+    input! {
+        n:usize,
+        a:[i64;n],
     }
-}
-
-/*
-
-            ▄▌▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
-     ▄▄██▌█            宅急便です！
-▄▄▄▌▐██▌█ Rating +25 :) をお届けに参りました！
-███████▌█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
-▀(⊙)▀▀▀▀(⊙)(⊙)▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀(⊙
-
-*/
-
-static INF: u64 = 1e18 as u64;
-
-trait ChLibs<T: std::cmp::Ord> {
-    fn chmin(&mut self, elm: T) -> bool;
-    fn chmax(&mut self, elm: T) -> bool;
-}
-
-impl<T: std::cmp::Ord> ChLibs<T> for T {
-    fn chmin(&mut self, elm: T) -> bool {
-        if *self > elm {
-            *self = elm;
-            true
-        } else {
-            false
-        }
+    let mut cur=0;
+    let mut v = vec![cur];
+    for ai in a{
+        cur+=ai;
+        v.push(cur%360);
     }
+    v.sort();
+    v.push(360);
+    println!("{}",v.iter().tuple_windows().map(|(&a, &b)|(a-b).abs()).max().unwrap());
 
-    fn chmax(&mut self, elm: T) -> bool {
-        if *self < elm {
-            *self = elm;
-            true
-        } else {
-            false
-        }
-    }
 }
-
-fn main() {
-    // input! { mut i: usize }
-    let mut i = 1;
-    while i != 0 {
-        solve();
-        i -= 1;
-    }
-}
-
