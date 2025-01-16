eaab984a8e6585c9c257529e81ab21e456abaf88
ardRiriy <hynnkgw27@gmail.com>
2025-01-16 23:50:58 +0900

    format

--- atcoder/abc238/src/bin/c.rs
+++ atcoder/abc238/src/bin/c.rs
@@ -1,6 +1,6 @@
 use ac_library::ModInt998244353;
-use proconio::{input};
-fn solve() {
+use proconio::input;
+fn main() {
     input!{
         n: u64
     }
@@ -10,7 +10,7 @@ fn solve() {
 
     let sum = |i| { i * (i + 1) / 2};
 
-    for i in 0.. {
+    for _ in 0.. {
         let upper = ModInt998244353::new((k - 1).min(n));
         // 1 から (upper - (10 ^ i - 1))の和を足す
         ans += sum(upper - (k / 10 - 1));
@@ -21,52 +21,4 @@ fn solve() {
         k *= 10;
     }
     println!("{}", ans);
-
 }
-
-/*
-
-            ▄▌▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
-     ▄▄██▌█            宅急便です！
-▄▄▄▌▐██▌█ Rating +25 :) をお届けに参りました！
-███████▌█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
-▀(⊙)▀▀▀▀(⊙)(⊙)▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀(⊙
-
-*/
-
-static INF: u64 = 1e18 as u64;
-
-trait ChLibs<T: std::cmp::Ord> {
-    fn chmin(&mut self, elm: T) -> bool;
-    fn chmax(&mut self, elm: T) -> bool;
-}
-
-impl<T: std::cmp::Ord> ChLibs<T> for T {
-    fn chmin(&mut self, elm: T) -> bool {
-        if *self > elm {
-            *self = elm;
-            true
-        } else {
-            false
-        }
-    }
-
-    fn chmax(&mut self, elm: T) -> bool {
-        if *self < elm {
-            *self = elm;
-            true
-        } else {
-            false
-        }
-    }
-}
-
-fn main() {
-    // input! { mut i: usize }
-    let mut i = 1;
-    while i != 0 {
-        solve();
-        i -= 1;
-    }
-}
-
