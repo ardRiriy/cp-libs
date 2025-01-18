c49efe832ae36e23e7d445379c64c9ae75ec7f81
ardririy <hynnkgw27@gmail.com>
2025-01-18 10:42:27 +0900

    各桁の寄与を考える

--- atcoder/abc233/src/bin/e.rs
+++ atcoder/abc233/src/bin/e.rs
@@ -1,53 +1,41 @@
-use proconio::{input};
-fn solve() {
-    input!{
-        
-    }
-}
-
-/*
-
-            ▄▌▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
-     ▄▄██▌█            宅急便です！
-▄▄▄▌▐██▌█ Rating +25 :) をお届けに参りました！
-███████▌█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
-▀(⊙)▀▀▀▀(⊙)(⊙)▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀(⊙
-
-*/
-
-static INF: u64 = 1e18 as u64;
-
-trait ChLibs<T: std::cmp::Ord> {
-    fn chmin(&mut self, elm: T) -> bool;
-    fn chmax(&mut self, elm: T) -> bool;
-}
-
-impl<T: std::cmp::Ord> ChLibs<T> for T {
-    fn chmin(&mut self, elm: T) -> bool {
-        if *self > elm {
-            *self = elm;
-            true
-        } else {
-            false
-        }
-    }
-
-    fn chmax(&mut self, elm: T) -> bool {
-        if *self < elm {
-            *self = elm;
-            true
-        } else {
-            false
-        }
-    }
-}
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    // input! { mut i: usize }
-    let mut i = 1;
-    while i != 0 {
-        solve();
-        i -= 1;
+    input! {
+        x: Chars
     }
+    let cs = x
+        .iter()
+        .map(|&c| c.to_digit(10).unwrap() as i32)
+        .rev()
+        .collect_vec();
+    let mut imos = vec![0; cs.len()+1];
+    for (i, xi) in cs.iter().enumerate() {
+        imos[0]+=xi;
+        imos[i+1]-=xi;
+    }
+    {
+        // flush
+        let mut cur = 0;
+        for i in 0..imos.len() {
+            cur+=imos[i];
+            imos[i]=cur;
+        }
+    }
+    let mut ans = vec![];
+    let mut kuriagari = 0;
+    for xi in imos {
+        kuriagari += xi;
+        ans.push(kuriagari%10);
+        kuriagari/=10;
+    }
+    while kuriagari!=0 {
+        ans.push(kuriagari%10);
+        kuriagari/=10;
+    }
+    while ans.len()>0&&*ans.last().unwrap()==0{
+        ans.pop();
+    }
+    println!("{}", ans.iter().rev().join(""));
 }
-
