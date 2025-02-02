f1cd61caafe9723f92190c47a1ef1e86bdda52bd
ardRiriy <hynnkgw27@gmail.com>
2025-02-02 18:25:21 +0900

    順番に書く (i+4)%8

--- atcoder/abc391/src/bin/a.rs
+++ atcoder/abc391/src/bin/a.rs
@@ -1,9 +1,10 @@
-#[allow(unused_imports)]
-use cps::debug::*;
 use proconio::input;
 
 fn main() {
     input!{
-
+        d: String
     }
+    let v = vec!["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
+    let i = v.iter().position(|&s| s.to_string() == d).unwrap();
+    println!("{}", v[(i+4)%v.len()]);
 }
