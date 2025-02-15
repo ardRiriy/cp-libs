9ad9a97eae5504af2ef6b45295025bc92b94295f
ardRiriy <hynnkgw27@gmail.com>
2025-02-15 10:04:56 +0900

    辺iを通る回数をWiとして、dp[i][j]]:=i本目の辺まで見たときに重みの和がjとなるような選び方の個数 としてdpに帰着

--- atcoder/abc222/src/bin/e.rs
+++ atcoder/abc222/src/bin/e.rs
@@ -1,75 +1,80 @@
-use proconio::{input};
-fn solve() {
-    input!{
-        
-    }
-}
+use std::collections::VecDeque;
 
-/*
-鹿のardririy, arDeeriy
-　　　　　　　　　　　　 　 　 　 　 　 　 ／
-　　　　　　　　　　　　　　　　　 　 　 //
-　　　　　　　　　 　 　 　 　 　 　 　 //　 　 　 　 |:
-　　　　　　　　　　　　　　　　　 　 // 　 　 　 　 .|i
-　　　　　　　　　 　 　 　 　 　 　 //　 　 　 　 　 ||
-　　　　　　　　　　　　　　　　　　l i　　　　　　 　 ||
-　　　　　.　´￣￣｀ｰ　、　　　　 | l　 　 　 　 　 ∥
-　　　 ／　 . 　 　 　_＞─- ､ 　 l |　　　　　　 .∥
-　　　i　 〆　 　 ／　 　 　 　 ＼.i | 　 　 　 　 ∥
-　　　| /　 　 ／ 　 　 　 　 　 　 l |　ﾊ　　　　.′
-　 　 ; :　 　 .′　　　　　　　　　 ヾゝi !　　　/.′
-　　 《　　　 :　　　 　 　 r─-､　　 i ﾘ:l　　 //　　　　 　 　 、
-　 　 |　　　　 　 　 　 　 ｀ヾ.　＼ r‐’:ﾚ=‐' .ゝ...ノ＿＿＼＿))
-　　 弋　 　 　 　 　 　 　 　 ∨ソ`ー''`ー---一　‐─‐-､)¨´
-　　 　 } 、　　　　　　　　　　 i_..ノ _,　　　　ハ'
-　　 　 ∨＞､　iゝ.. 　 　 　 　 　 弋);;,ゞ..ノ　.′
-　　　　 ∨::::| 7!:::..ヾ.　　　　.′ヽ.　` 　 　 /
-　　　　　∨::|.′::::::ixxr, 　 /　　　:　　　　 I.
-　 　 　 　 }:::|..:ｉ..::::::|　i,,　　"'' ´ヾ.i 　 　 　 ﾊ
-　　　　 　 ::::|:::|::::::/　 }ヾ.　　　　ﾐゝ.　 ,、..:::::)
-　　　　 　 i:::|ヾ:::::i　 /.::::|｀ヾ..,,　..ノ＼ヾ..＿/
-　 　 　 　 |:::l　}:. {　 |::::..′　　　　　　 `ー''
-　 　 　 　 |:::| /.:/l　 !.:::l
-　　　　　 ﾉ..:ﾚ.:::ｉ::l　ﾉ..::|
-　　　 　 (人7.::::|:::V.::::::|
-　　　　　`''/ .:::::!ｰ:::::::::ﾊ
-　　　　　 /..::::::::| (_/＼__)
-　　　 　 (__/(,＿)
-*/
+use ac_library::ModInt998244353 as Mint;
+use itertools::Itertools;
+use proconio::{input, marker::Usize1};
 
-static INF: u64 = 1e18 as u64;
-
-trait ChLibs<T: std::cmp::Ord> {
-    fn chmin(&mut self, elm: T) -> bool;
-    fn chmax(&mut self, elm: T) -> bool;
-}
-
-impl<T: std::cmp::Ord> ChLibs<T> for T {
-    fn chmin(&mut self, elm: T) -> bool {
-        if *self > elm {
-            *self = elm;
-            true
-        } else {
-            false
-        }
-    }
-
-    fn chmax(&mut self, elm: T) -> bool {
-        if *self < elm {
-            *self = elm;
-            true
-        } else {
-            false
-        }
-    }
-}
+static INF: usize = 1<<60;
 
 fn main() {
-    // input! { mut i: usize }
-    let mut i = 1;
-    while i != 0 {
-        solve();
-        i -= 1;
+    input!{
+        n: usize,
+        m: usize,
+        k: i64,
+        a: [Usize1; m],
+        e: [(Usize1, Usize1); n-1],
     }
-}
 
+    let g = e.iter()
+        .enumerate()
+        .fold(vec![vec![]; n], |mut g, (i, &(u, v))| {
+            g[u].push((v, i));
+            g[v].push((u, i));
+            g
+        });
+
+    let mut w = vec![0; n-1];
+    for (&cur, &np) in a.iter().tuple_windows() {
+        if cur == np {
+            continue;
+        }
+
+        let mut seen = vec![(INF, INF); n];
+        seen[cur] = (!0, !0);
+        let mut que = VecDeque::new();
+        que.push_back(cur);
+        while let Some(p) = que.pop_front() {
+            if p == np {
+                break;
+            }
+            for &(ni, i) in g[p].iter() {
+                if seen[ni].0 == INF {
+                    seen[ni] = (p, i);
+                    que.push_back(ni);
+                }
+            }
+        }
+
+        {
+            let mut p = np;
+            while seen[p].0 != !0 {
+                w[seen[p].1] += 1;
+                p = seen[p].0;
+            }
+        }
+    }
+
+    let sum = w.iter().sum::<u64>();
+    let x = sum as i64 + k;
+    if x%2==1 || x < 0 {
+        println!("0");
+        return;
+    }
+
+    let x = x as usize;
+    let mut dp = vec![vec![Mint::new(0); x+1]; 2];
+    let mut cur = 0;
+    dp[cur][0] = Mint::new(1);
+    for i in 0..n-1 {
+        let nxt = 1-cur;
+        for j in 0..x+1 {
+            if w[i] as usize + j < x+1 {
+                dp[nxt][w[i] as usize + j] = dp[nxt][w[i] as usize + j] + dp[cur][j];
+            }
+            dp[nxt][j] = dp[nxt][j] + dp[cur][j];
+        }
+        dp[cur].fill(Mint::new(0));
+        cur = nxt;
+    }
+    println!("{}", dp[cur][x/2]);
+}
