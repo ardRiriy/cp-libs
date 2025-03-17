66dc7fbe22396d5e9b6dde1bbea1ea12f2a5e6f7
ardRiriy <hynnkgw27@gmail.com>
2025-03-17 21:51:35 +0900

    dp[i] := iを最後に選ぶときの選んだビルの最大としてdp
    i,j (i<j)のペアをi -> jの傾きでソートして、その順で更新していくと自然に残すビルの条件を満たす

--- user_contests/oupc2024_day1/o.cpp
+++ user_contests/oupc2024_day1/o.cpp
@@ -27,77 +27,45 @@ template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return true; } r
 ll dx[] = {1, 0, -1, 0, -1, 1, -1, 1};
 ll dy[] = {0, 1, 0, -1, -1, 1, 1, -1};
 
-/* RMQ：[0,n-1] について、区間ごとの最小値を管理する構造体
-    update(i,x): i 番目の要素を x に更新。O(log(n))
-    query(a,b): [a,b) での最小の要素を取得。O(log(n))
-*/
-template <typename T>
-struct RMQ {
-    const T INF = numeric_limits<T>::min();
-    int n;         // 葉の数
-    vector<T> dat; // 完全二分木の配列
-    RMQ(int n_) : n(), dat(n_ * 4, INF) { // 葉の数は 2^x の形
-        int x = 1;
-        while (n_ > x) {
-            x *= 2;
-        }
-        n = x;
-    }
-
-    void update(int i, T x) {
-        i += n - 1;
-        dat[i] = x;
-        while (i > 0) {
-            i = (i - 1) / 2;  // parent
-            dat[i] = max(dat[i * 2 + 1], dat[i * 2 + 2]);
-        }
-    }
-
-    // the minimum element of [a,b)
-    T query(int a, int b) { return query_sub(a, b, 0, 0, n); }
-    T query_sub(int a, int b, int k, int l, int r) {
-        if (r <= a || b <= l) {
-            return INF;
-        } else if (a <= l && r <= b) {
-            return dat[k];
-        } else {
-            T vl = query_sub(a, b, k * 2 + 1, l, (l + r) / 2);
-            T vr = query_sub(a, b, k * 2 + 2, (l + r) / 2, r);
-            return max(vl, vr);
-        }
-    }
-};
-
-
-bool compare1(const pair<ll, pair<ll, ll>>& p1, const pair<ll, pair<ll, ll>>& p2) {
-   return  p1.second.second * (p2.second.first) <= p1.second.first * (p2.second.second);
-}
-
-
 void solve() {
     int n; cin >> n;
-    vll x(n+1), h(n+1),c(n+1);
-    x[0] = 0; h[0] = -1000, c[0] = 0;
-    rep(i, n) cin >> x[i+1];
-    rep(i, n) cin >> h[i+1];
-    rep(i, n) cin >> c[i+1];
-
+    ll s = 0;
+    vll x(n), h(n),c(n);
+    rep(i, n) cin >> x[i];
+    rep(i, n) cin >> h[i];
+    rep(i, n) { cin >> c[i]; s += c[i]; }
     
     if(n==1) {
         cout << "0\n";
         return;
     }
-    
+
+    vector<pair<int, int>> v;
+    rep(i, n) rep2(j, i+1, n) v.push_back({i, j});
+    stable_sort(all(v), [&](const P & a, const P & b) {
+        // 傾きが小さい順に並べる
+        auto [xi, xj] = a;
+        auto [yi, yj] = b;
+
+        ll l = (h[xj] - h[xi]) * (x[yj] - x[yi]);
+        ll r = (x[xj] - x[xi]) * (h[yj] - h[yi]);
+        if(l == r) return xj < yj;
+        else return l < r;
+    });
+
+    vll maximam(n, 0);
+    rep(i, n) maximam[i] = c[i];
+    for(auto [i, j]: v) {
+        chmax(maximam[j], maximam[i] + c[j]);
+    }
+
     ll ans = 0;
-    // pair first -> numer second -> denom
-    // dp i j := 最後にi, j番目のビルを残すと決めた時に「残すビルのコストCの総和」の最大値
-    
+    rep(i, n) chmax(ans, maximam[i]);
+    cout << s - ans << '\n';
 }
 
 int main() {
     cin.tie(0);
     ios::sync_with_stdio(false);
-    int t=1;
-    //cin >> t;
-    while(t--)solve();
+    solve();
 }
