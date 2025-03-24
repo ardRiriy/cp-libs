06ee270a41cff51e1f7e2c6fcce5bb39addab2cc
ardririy <hynnkgw27@gmail.com>
2025-03-24 10:57:14 +0900

    前後から見る

---
+++ atcoder/arc195/a.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+
+#define all(v) v.begin(),v.end()
+#define resort(v) sort(v.rbegin(),v.rend())
+using ll = long long;
+using ull = unsigned long long;
+using vll=vector<ll>;
+using vvll = vector<vector<ll>>;
+using P = pair<ll,ll>;
+using vp=vector<pair<ll, ll>>;
+using djks=priority_queue<P, vp, greater<P>>;
+
+const ll inf=1ll<<60;
+#define mod10 (ll)1e9+7
+#define mod99 (ll)998244353
+const double PI = acos(-1);
+
+#define rep(i,n) for (ll i=0;i<n;++i)
+#define per(i,n) for(ll i=n-1;i>=0;--i)
+#define rep2(i,a,n) for (ll i=a;i<n;++i)
+#define per2(i,a,n) for (ll i=n-1;i>=a;--i)
+
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return true; } return false; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return true; } return false; }
+
+ll dx[] = {1, 0, -1, 0, -1, 1, -1, 1};
+ll dy[] = {0, 1, 0, -1, -1, 1, 1, -1};
+
+void solve() {
+    int n, m; cin >> n >> m;
+    vll a(n), b(m); rep(i, n) cin >> a[i]; rep(i, m) cin >> b[i];
+
+    vll f, l;
+
+    int cur = 0;
+    rep(i, n) {
+        if(cur < m && a[i] == b[cur]) {
+            f.push_back(i);
+            cur++;
+        }
+    }
+    cur = m-1;
+    per(i, n) {
+        if(cur >= 0 && a[i] == b[cur]) {
+            l.push_back(i);
+            cur--;
+        }
+    }
+    reverse(all(l));
+    if(f.size() == m && l.size() == m && f != l) {
+        cout << "Yes\n";
+    } else {
+        cout << "No\n";
+    }
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int t=1;
+    //cin >> t;
+    while(t--)solve();
+}
+
+
