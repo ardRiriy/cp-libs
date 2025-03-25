ec5b84f38fbb38156808c87bf090165c36eefc4a
ardririy <hynnkgw27@gmail.com>
2025-03-25 18:48:06 +0900

    仮想配列を使って言われた通りに

---
+++ atcoder/abc398/src/bin/c.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) v.begin(),v.end()
+#define resort(v) sort(v.rbegin(),v.rend())
+using ll = long long;
+using ull = unsigned long long;
+using vll=vector<ll>;
+using vvll = vector<vector<ll>>;
+using P = pair<ll,ll>;
+using vp=vector<pair<ll, ll>>;
+using djks=priority_queue<P, vp, greater<P>>;
+
+const ll inf=1ll<<60;
+#define mod10 (ll)1e9+7
+#define mod99 (ll)998244353
+const double PI = acos(-1);
+
+#define rep(i,n) for (ll i=0;i<n;++i)
+#define per(i,n) for(ll i=n-1;i>=0;--i)
+#define rep2(i,a,n) for (ll i=a;i<n;++i)
+#define per2(i,a,n) for (ll i=n-1;i>=a;--i)
+
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return true; } return false; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return true; } return false; }
+
+ll dx[] = {1, 0, -1, 0, -1, 1, -1, 1};
+ll dy[] = {0, 1, 0, -1, -1, 1, 1, -1};
+
+void solve() {
+    int n; cin >> n;
+    vll a(n); rep(i, n) cin >> a[i];
+
+    map<ll, ll> cnt;
+    for(auto ai: a) {
+        cnt[ai]++;
+    }
+
+    ll ans = -1;
+    ll mx = -1;
+    rep(i, n) {
+        if(cnt[a[i]] == 1) {
+            if(chmax(mx, a[i])) {
+                ans = i+1;
+            }
+        }
+    }
+
+    cout << ans << '\n';
+    
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int t=1;
+    //cin >> t;
+    while(t--)solve();
+}
+
+
