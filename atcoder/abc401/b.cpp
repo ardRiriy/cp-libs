bf2a9672d5845339a455a09afd0d8872e8bf1138
ardRiriy <hynnkgw27@gmail.com>
2025-04-13 14:20:49 +0900

    login済みかどうかをboolで管理

---
+++ atcoder/abc401/b.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+
+#define all(v) v.begin(),v.end()
+#define resort(v) sort(v.rbegin(),v.rend())
+using ll = long long;
+using ull = unsigned long long;
+using vll=vector<ll>;
+using vvll = vector<vector<ll>>;
+using P = pair<ll,ll>;
+using vp=vector<pair<ll, ll>>;
+using djks=priority_queue<P, vp, greater<P>>;
+
+const ll inf=1ll<<60;
+#define mod10 (ll)1e9+7
+#define mod99 (ll)998244353
+const double PI = acos(-1);
+
+#define rep(i,n) for (ll i=0;i<n;++i)
+#define per(i,n) for(ll i=n-1;i>=0;--i)
+#define rep2(i,a,n) for (ll i=a;i<n;++i)
+#define per2(i,a,n) for (ll i=n-1;i>=a;--i)
+
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return true; } return false; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return true; } return false; }
+
+ll dx[] = {1, 0, -1, 0, -1, 1, -1, 1};
+ll dy[] = {0, 1, 0, -1, -1, 1, 1, -1};
+
+void solve() {
+    int n; cin >> n;
+    bool il = false;
+    ll cnt = 0;
+    rep(i,n){
+        string s; cin >> s;
+        if(s == "login") {
+            il = true;
+        } else if(s=="logout") {
+            il = false;
+        } else if(s=="public") {
+
+        } else {
+            if(!il)
+            cnt++;
+        }
+    }
+    cout << cnt << '\n';
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int t=1;
+    //cin >> t;
+    while(t--)solve();
+}
+
+
