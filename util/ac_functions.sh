be8db75e3d5ddbe28d0431426abb45329d34835b
ardRiriy <hynnkgw27@gmail.com>
2025-04-20 01:42:41 +0900

    util update

--- util/ac_functions.sh
+++ util/ac_functions.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 gpp(){
-	g++ $1 -std=c++23 -O2 -W -Wall || return 1
+	g++ $1 -std=c++23 -W -Wall -fsanitize=undefined
 	./a.out
 }
 
@@ -12,29 +12,18 @@ acsub() {
     if [ $1 = "rs" ]; then
         uv run oj t -c "cargo run --features local --bin $2" || return 1
         uv run python3 ../../util/file_merger.py $2
-        uv run oj s $3 src/bin/$2.rs -y
+        # uv run oj s $3 src/bin/$2.rs -y
         uv run python3 ../../util/after_submission.py $2
     elif [ $1 = "py" ]; then
         uv run oj t -c "uv run python3 src/bin/$2.py" || return 1
         # pypyで提出
-        uv run oj s $3 src/bin/$2.py -l 5078
+        # uv run oj s $3 src/bin/$2.py -l 5078
     elif [ $1 = "cpp" ]; then
-	g++ $2.cpp -std=c++23 -O2 -W -Wall
-	uv run oj t || return 1
-	uv run oj s $3 $2.cpp
-    else
-        echo "language $1 is not exist!"
-    fi
-}
-
-actest() {
-    echo "usage: acsub <language> <problem> <url>"
-    rm -rf test
-    uv run oj d $3
-    if [ $1 = "rs" ]; then
-        uv run oj t -c "cargo run --features local --bin $2" || return 1
-    elif [ $1 = "py" ]; then
-        uv run oj t -c "uv run python3 src/bin/$2.py" || return 1
+        g++ $2.cpp -std=c++23 -O2 -W -Wall
+        uv run oj t || return 1
+        # uv run oj s $3 $2.cpp
+	uv run oj-bundle -I/home/ardririy/repos/cp-libs/libraries-cpp -I/usr/local/include/ $2.cpp | clip
+	echo "\nAll Tests passed. Code was copied to clipboard!"
     else
         echo "language $1 is not exist!"
     fi
