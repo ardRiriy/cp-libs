b8df239960ddc3402f83f7598d38e31377d4a4f4
ardRiriy <hynnkgw27@gmail.com>
2025-05-02 10:57:09 +0900

    簡単なdp

---
+++ icpc/domestic_japan/2022_jag/b.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(v) v.begin(),v.end()
+#define resort(v) sort(v.rbegin(),v.rend())
+using ll = long long;
+using ull = unsigned long long;
+using vll=vector<ll>;
+using vvll = vector<vector<ll>>;
+using P = pair<ll,ll>;
+using vp=vector<pair<ll, ll>>;
+using djks=priority_queue<P, vp, greater<P>>;
+
+const ll inf=1ll<<60;
+#define mod10 (ll)1e9+7
+#define mod99 (ll)998244353
+const double PI = acos(-1);
+
+#define rep(i,n) for (ll i=0;i<(n);++i)
+#define per(i,n) for(ll i=(n)-1;i>=0;--i)
+#define rep2(i,a,n) for (ll i=(a);i<(n);++i)
+#define per2(i,a,n) for (ll i=(n)-1;i>=(a);--i)
+
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return true; } return false; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return true; } return false; }
+
+ll dx[] = {1, 0, -1, 0, -1, 1, -1, 1};
+ll dy[] = {0, 1, 0, -1, -1, 1, 1, -1};
+
+string s;
+void solve(ll k) {
+    cin >> s;
+    int n = s.size();
+
+    vll base(k, 1);
+    rep(i,k-1) base[i+1] = base[i]*10;
+
+    vvll dp(n+1,vll(k, -inf));
+    dp[0][0] = 0;
+
+    rep(i,n){
+        ll ai = s[i]-'0';
+        rep(j,k){
+            chmax(dp[i+1][j], dp[i][j]); // 捨てる
+
+            chmax(dp[i+1][(j+k-1)%k], dp[i][j] + base[(j+k-1)%k]*ai);
+        }
+    }
+    cout << dp[n][0] << '\n';
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int k;cin>>k;
+    while(k>0){
+        solve(k);
+        cin>>k;
+    }
+}
